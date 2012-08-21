83f98b0bb1f86747d5e32d96e49067aa62ff966e
Shaya Krauss <shaya4207@gmail.com>
2012-08-20 23:33:40 -0400

    Started creating site!!!
    Finally!!!

---
+++ newsite/index.php
@@ -0,0 +1,15 @@
+<?php
+	$file = $_SERVER['HTTP_HOST'].dirname($_SERVER['SCRIPT_NAME']);
+	$root = dirname(__FILE__);
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<link rel="stylesheet" type="text/css" href="http://<?php echo $file?>/_ext/default.css" />
+</head>
+
+<body>
+</body>
+</html>
