10cbdbf0d58cb187721fb8089a0a580b665c9e14
Shaya Krauss <shaya4207@gmail.com>
2012-08-21 18:16:22 -0400

    Started actually coding ;)

--- newsite/index.php
+++ newsite/index.php
@@ -4,12 +4,26 @@
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
-<link rel="stylesheet" type="text/css" href="http://<?php echo $file?>/_ext/default.css" />
-</head>
-
-<body>
-</body>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>Untitled Document</title>
+		<link rel="stylesheet" type="text/css" href="http://<?php echo $file?>/_ext/default.css" />
+	</head>
+	<body>
+		<div class="main_wrapper">
+			<div class="main_nav">
+				<ul>
+					<li><a href=""></a></li>
+					<li><a href=""></a></li>
+					<li><a href=""></a></li>
+					<li><a href=""></a></li>
+					<li><a href=""></a></li>
+				</ul>
+			</div>
+			<div class="content">
+				test
+			</div>
+			<div class="clear">&nbsp;</div>
+		</div>
+	</body>
 </html>
