c63819ce36839652ac41d922767015f4677f03a9
Shaya Krauss <shaya4207@gmail.com>
2012-11-26 18:55:37 -0500

    ...

--- 0.1/parts/foot.php
+++ 0.1/parts/foot.php
@@ -2,11 +2,15 @@
     <script>window.jQuery || document.write('<script src="js/js/libs/jquery-1.7.1.min.js"><\/script>')</script>
     <script src="js/js/plugins.js"></script>
     <script src="js/js/script.js"></script>
-    <script>
-      var _gaq=[['_setAccount','UA-36457791-1'],['_trackPageview']];
-      (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-      g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-      s.parentNode.insertBefore(g,s)}(document,'script'));
+    <script type="text/javascript">
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-36457791-1']);
+      _gaq.push(['_trackPageview']);
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
     </script>
   </body>
 </html>
